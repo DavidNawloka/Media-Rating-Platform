d838b59ec1c8442ef546ea4f8a3614046adb30b7
David Nawloka <nawlokadavid@gmail.com>
2025-10-19 19:47:21 +0200

    Removed debug print

--- src/main/java/at/fhtw/swen1/controller/MediaController.java
+++ src/main/java/at/fhtw/swen1/controller/MediaController.java
@@ -35,7 +35,6 @@ public class MediaController extends Controller {
             }
 
             if(method.equals("GET")){
-                System.out.println("GET media");
                 handleGetMedia(exchange,mediaId);
             }
 
